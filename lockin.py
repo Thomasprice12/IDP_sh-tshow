3654b35fe1fbed8c0a03e9df0fcc7cb43ef5d280
Thomasprice12 <tp565@cam.ac.uk>
2025-11-19 09:55:24 +0000

    enjoy pranoy

--- lockin.py
+++ lockin.py
@@ -29,14 +29,17 @@ s1 = Pin(8,  Pin.IN)   # leftmost
 s2 = Pin(9,  Pin.IN)
 s3 = Pin(10, Pin.IN)
 s4 = Pin(11, Pin.IN)   # rightmost
-
-
-
-
+LL= s1.value()
+L=   s2.value()
+R = s3.value()
+RR = s4.value()
 
 def go_straight(speed=FORWARD_SPEED):
     left_motor.Forward(speed)
     right_motor.Forward(speed)
+def_adjust(left_speed=x, right_speed=y):
+    left_motor.forward(left_speed)
+    right_motor.forward(right_speed)
 
 def stop():
     left_motor.off()
@@ -63,7 +66,26 @@ def uturn():
     turn_left_90()
 
 def read():
-    return (s1.value(), s2.value(), s3.value(), s4.value())
+    return (LL, L, R,RR)
+'''Black = 0, white = 1'''
 
+'''block of code below should deaL with the 1st 2 junctions '''
+counter = 0 
+go_straight(100)
+while true:
+    if read() = (1,1,1,1):
+        counter +=1
+        if counter ==2:
+            turn_right_90()
+'''this deals with anything onwards. Pranoy, you need to add a function to deal with dead end bays'''            
+while true:
+    read()
+    if read() == (0,1,1,0):
+        go_straight(100)
+    elif read() == (0,0,1,0):
+        right_motor.Forward(70) # basically slow down right motor so turns to right
+    elif read() == (0,1,0,0):
+        left_motor.Forward(70) # likewise for left side down so turns to the left
+    elif read() == (1,1,1,0):
+        turn_left_90()		   # this will turn down every left hand junction i.e. the bays 
 
-# ---------------- Line Follow ----------------
