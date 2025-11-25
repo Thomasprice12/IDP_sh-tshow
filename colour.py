7e8c2507f309aa5dba8026f671157c59bca36c11
kg559 <kg559@cam.ac.uk>
2025-11-25 09:16:28 +0000

    Update colour.py

--- colour.py
+++ colour.py
@@ -1,1 +1,15 @@
+from machine import Pin, SoftI2C, I2C
+from libs.tcs3472_micropython.tcs3472 import tcs3472
+from utime import sleep
+
+colourpin = Pin(17, 
+
+
+def colour_detect():
+    
+    i2c_bus = I2C(id=1, sda=Pin(18), scl=Pin(19)) 
+    print(i2c_bus.scan())
+    tcs = tcs3472(i2c_bus)
+    while True:
+        print("Light:", tcs.light())
 
