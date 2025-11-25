33f3db42b38e63c4126d78814d7c8d468b9d2485
kg559 <kg559@cam.ac.uk>
2025-11-25 09:27:21 +0000

    Update colour detection and pin initialization
    Refactor colour detection logic and initialize colourpin.

--- colour.py
+++ colour.py
@@ -2,14 +2,28 @@ from machine import Pin, SoftI2C, I2C
 from libs.tcs3472_micropython.tcs3472 import tcs3472
 from utime import sleep
 
-colourpin = Pin(17, 
-
-
+colourpin = Pin(17, Pin.Out)
+colourpin.value(1)
 def colour_detect():
-    
+    colour = "none"
+    colourpin.value(0)
     i2c_bus = I2C(id=1, sda=Pin(18), scl=Pin(19)) 
-    print(i2c_bus.scan())
     tcs = tcs3472(i2c_bus)
-    while True:
-        print("Light:", tcs.light())
+    x = 0
+    for i in range(0,5):
+        x += tcs.light()
+    x = x/5
+    if 3000 > x > 2000:
+        colour = "red"
+    elif 2000 > x > 1400:
+        colour = "green"
+    elif 5000 > x > 3000:
+        colour = "blue" 
+    elif x > 5000:
+        colour = "yellow"
+    else 
+        colour = "none"
+    return colour
+        
+        
 
