e752c520c151ec4624fc982a4368993d2a2df2de
kg559 <kg559@cam.ac.uk>
2025-11-25 10:28:09 +0000

    Refactor colour detection sampling and I2C setup
    Increased the number of samples taken for color detection from 5 to 20 and adjusted the I2C initialization.

--- colour.py
+++ colour.py
@@ -2,17 +2,17 @@ from machine import Pin, SoftI2C, I2C
 from libs.tcs3472_micropython.tcs3472 import tcs3472
 from utime import sleep
 
+i2c_bus = I2C(id=1, sda=Pin(18), scl=Pin(19), freq = 10) 
+tcs = tcs3472(i2c_bus)
 colourpin = Pin(17, Pin.Out)
 colourpin.value(1)
 def colour_detect():
     colour = "none"
     colourpin.value(0)
-    i2c_bus = I2C(id=1, sda=Pin(18), scl=Pin(19)) 
-    tcs = tcs3472(i2c_bus)
     x = 0
-    for i in range(0,5):
+    for i in range(0,20):
         x += tcs.light()
-    x = x/5
+    x = x/20
     if 3000 > x > 2000:
         colour = "red"
     elif 2000 > x > 1400:
